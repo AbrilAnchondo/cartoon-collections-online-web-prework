afb9faba47ba765f97069c3e111ede8d28baa9dd
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-04 03:01:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,6 @@ end
 def summon_captain_planet(calls)# code an argument here
   # Your code here
     calls.map {|x|  x.capitalize + "!"}
-    
 end
 
 def long_planeteer_calls# code an argument here
