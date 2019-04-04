a2fa9fa577e6480b8eab2c63582fd343ed0801c7
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-04 03:16:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,6 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
  cheese_types.first 
- if array.include?
+ if array.include?(cheese_types) == false 
+   return nil
 end
